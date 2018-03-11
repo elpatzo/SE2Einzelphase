ef584a67ccd63d7f37e8ddd8925e2d8a843e765d
elpatzo <elpatzo@github.com>
2018-03-11 16:20:58 +0100

    final_commit

--- app/src/main/java/com/example/patrick/se2einzelphase/MainActivity.java
+++ app/src/main/java/com/example/patrick/se2einzelphase/MainActivity.java
@@ -9,7 +9,31 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
+    public void checkPalindrom(View view){
+        EditText edit = (EditText) findViewById(R.id.txtEnter);
+        TextView result = (TextView) findViewById(R.id.txtResult);
+        String pal = edit.getText().toString();
 
+        if (pal.length() == 0){
+            result.setText("Kein String eingegeben");
+        }
+        else if (pal.length() <5){
+            result.setText("String zu kurz");
+        }
+        else{
+            if(isPalindrome(pal)){
+                result.setText("ist Palindrom");
+            }
+            else {
+                result.setText("kein Palindrom");
+            }
+        }
+    }
+
+
+    public static boolean isPalindrome(String str) {
+        return str.equals(new StringBuilder(str).reverse().toString());
+    }
 
 
     @Override
