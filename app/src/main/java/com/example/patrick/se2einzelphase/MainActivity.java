7fbd72af370b7520ce85d5b5270355a2b11e412e
elpatzo <elpatzo@github.com>
2018-03-11 16:15:29 +0100

    ui_erstellt

--- app/src/main/java/com/example/patrick/se2einzelphase/MainActivity.java
+++ app/src/main/java/com/example/patrick/se2einzelphase/MainActivity.java
@@ -2,8 +2,15 @@ package com.example.patrick.se2einzelphase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
